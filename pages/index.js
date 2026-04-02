8620309e7092f74393b4a7a80be239525d25b4c8
AFONSO HENRIQUE <afhenriqueoliveira@gmail.com>
2026-04-02 17:35:12 +0000

    Adcionar scripts prettier

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 function Home() {
-  return <h1>Programador não bebe café para acordar.
-    Ele bebe café para compilar a alma</h1>
+  return (
+    <h1>
+      Programador não bebe café para acordar. Ele bebe café para compilar a alma
+    </h1>
+  );
 }
-
 
 export default Home;
