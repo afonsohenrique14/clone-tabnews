e46dcc3338e3fe421cfbbebbdef2fcd80c605c44
AFONSO HENRIQUE <afhenriqueoliveira@gmail.com>
2026-04-02 17:06:13 +0000

    adiciona arquivo `.editorconfig`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 function Home() {
-    return <h1>Programador não bebe café para acordar.  
-      Ele bebe café para compilar a alma</h1>
+  return <h1>Programador não bebe café para acordar.
+    Ele bebe café para compilar a alma</h1>
 }
+
 
 export default Home;
