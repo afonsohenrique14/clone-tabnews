2c0b319c7551e3ecc8ba46e762e295267679125c
AFONSO HENRIQUE <afhenriqueoliveira@gmail.com>
2026-04-12 12:31:40 +0000

    makes `database.js` client.connect() more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -8,11 +8,20 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
 
+  console.log("Credenciais do Postgres:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+
   try {
     await client.connect();
     return await client.query(queryObject);
   } catch (err) {
     console.error(err);
+    throw error;
   } finally {
     await client.end();
   }
